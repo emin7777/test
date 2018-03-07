8a831a17ec8d80b82719af8f0b77534b4ba6b41b
emin7777 <eminnh@code.edu.az>
2018-03-07 09:05:46 +0400

    finished

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir eded daxil edin:");
+            var eded = Convert.ToInt32(Console.ReadLine());
+            cutEded(eded);
+
+            void cutEded(int _eded)
+            {
+                if (_eded%2 == 0)
+                {
+                    Console.WriteLine("cut ededir");
+                }
+            }
+        }
+    }
+}
